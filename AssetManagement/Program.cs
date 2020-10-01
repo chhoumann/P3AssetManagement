6ad4c323462c3150f3d1fc336e02e3d93bad3d0f
Pattrigue <Pattrigue@users.noreply.github.com>
2020-10-01 14:52:24 +0200

    Update Program.cs

--- AssetManagement/Program.cs
+++ AssetManagement/Program.cs
@@ -18,11 +18,12 @@ namespace AssetManagement
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        }
     }
 }
