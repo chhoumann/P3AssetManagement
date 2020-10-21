a57d796e92dc9016fead333a18297bcad1956af7
Pattrigue <pfas19@student.aau.dk>
2020-10-21 11:01:04 +0200

    Move private Asset class to partial AssetController class

---
+++ AssetManagement.Models/AssetController.Asset.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssetManagement.Models
+{
+    public static partial class AssetController
+    {
+        private sealed class Asset : IAsset
+        {
+            public string Model { get; }
+            public string SerialNumber { get; }
+
+            public int Id { get; }
+
+            public DateTime LastChanged { get; private set; }
+
+            public AssetHolder CurrentAssetHolder { get; private set; }
+
+            public List<StateRecord> Records { get; } = new List<StateRecord>();
+            public List<Transaction> Transactions { get; } = new List<Transaction>();
+
+            public Asset(int id, string name, string serialNumber)
+            {
+                Model = name;
+                SerialNumber = serialNumber;
+                Id = id;
+
+                Records.Add(new StateRecord(AssetState.Online)); 
+            }
+
+            public void TransferTo(AssetHolder newAssetHolder)
+            {
+                // Remove this asset from its current holder, if any
+                if (CurrentAssetHolder != null)
+                {
+                    if (!CurrentAssetHolder.CurrentAssets.Contains(this))
+                    {
+                        // ERROR: This asset does not exist in the CurrentAssetHolder's list so we cannot remove it!
+                        throw new ArgumentException($"Attempt to remove asset \"{Model}\" from an asset holder's asset list that did not contain the asset!");
+                    }
+
+                    CurrentAssetHolder.CurrentAssets.Remove(this);
+                }
+
+                if (newAssetHolder.CurrentAssets.Contains(this))
+                {
+                    // ERROR: New holder is somehow already holding this asset!
+                    throw new ArgumentException("Attempt to add an asset to an asset holder's asset list which already contains this asset!", Model);
+                }
+
+                // Update the current holder by transferring this asset to the new AssetHolder
+                newAssetHolder.CurrentAssets.Add(this);
+                CurrentAssetHolder = newAssetHolder;
+            }
+
+            public void Dispose() { }
+        }
+    }
+}
