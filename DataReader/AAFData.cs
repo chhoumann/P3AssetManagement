78451fd9f1bf392bab3296be5c840ef7dcccc811
Laurits <laurits.mumberg@gmail.com>
2020-10-21 13:21:03 +0200

    CSVParser-class tilføjet, testet med vores mockdata.

---
+++ DataReader/AAFData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DataReader
+{
+    class AAFData
+    {
+        public DateTime Timestamp { get; private set; }
+        public string Pcnavn { get; private set; }
+        public string Primaerbruger { get; private set; }
+        public string Navn { get; private set; }
+        public string Afdeling { get; private set; }
+        public string Operativsystem { get; private set; }
+        public int CPU { get; private set; }
+        public int RAM { get; private set; }
+        public int HDD { get; private set; }
+        public string Servicepack { get; private set; }
+        public string Serienummer { get; private set; }
+        public DateTime Fakturadato { get; private set; }
+        public string Gateway { get; private set; }
+        public string Model { get; private set; }
+        public string Pctype { get; private set; }
+
+        public AAFData(DateTime timestamp, string pcnavn, string primaerbruger, string navn, string afdeling, string operativsystem, int cPU, int rAM, int hDD, string servicepack, string serienummer, DateTime fakturadato, string gateway, string model, string pctype)
+        {
+            Timestamp = timestamp;
+            Pcnavn = pcnavn;
+            Primaerbruger = primaerbruger;
+            Navn = navn;
+            Afdeling = afdeling;
+            Operativsystem = operativsystem;
+            CPU = cPU;
+            RAM = rAM;
+            HDD = hDD;
+            Servicepack = servicepack;
+            Serienummer = serienummer;
+            Fakturadato = fakturadato;
+            Gateway = gateway;
+            Model = model;
+            Pctype = pctype;
+        }
+    }
+}
---
+++ DataReader/CSVParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataReader
+{
+    class CSVParser
+    {
+        public List<AAFData> LoadAAFDataFromCSV(string path)
+        {
+            List<AAFData> data = new List<AAFData>();
+            StreamReader sr;
+            try
+            {
+                sr = new StreamReader(path);
+            }
+            catch
+            {
+
+                throw new FileNotFoundException("File not found on given path");
+            }
+            
+            
+            // First line is skipped, as it includes headers and has no actual data.
+            string headerLine = sr.ReadLine();
+
+            // Reads all lines in csv, and saves them in an AAFData list.
+            string currentLine;
+            while ((currentLine = sr.ReadLine()) != null)
+            {
+                data.Add(AAFDataFromCSVString(currentLine));
+            }
+            
+            return data;
+        }
+
+        private AAFData AAFDataFromCSVString(string csvLine)
+        {
+            string[] csvFields = csvLine.Split(';');
+            try
+            {
+                // Parses the string-array to an AAFData object
+                AAFData parsedData = new AAFData(DateTime.Parse(csvFields[0]),
+                    csvFields[1],
+                    csvFields[2],
+                    csvFields[3],
+                    csvFields[4],
+                    csvFields[5],
+                    Int32.Parse(csvFields[6]),
+                    Int32.Parse(csvFields[7]),
+                    Int32.Parse(csvFields[8]),
+                    csvFields[9],
+                    csvFields[10],
+                    DateTime.Parse(csvFields[11]),
+                    csvFields[12],
+                    csvFields[13],
+                    csvFields[14]
+                    );
+
+                return parsedData;
+            }
+            catch 
+            {
+                throw new FormatException("Field in data was not formatted correctly, and could not be parsed");
+            }
+
+        }
+    }
+}
