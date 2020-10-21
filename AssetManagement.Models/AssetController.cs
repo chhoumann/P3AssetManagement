47b1cb9e7e9276df662b6d587fc273a0efe9f9ff
Pattrigue <pfas19@student.aau.dk>
2020-10-21 11:04:04 +0200

    Move AssetService Mockdata back to AssetService

--- AssetManagement.Models/AssetController.cs
+++ AssetManagement.Models/AssetController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace AssetManagement.Models
+﻿namespace AssetManagement.Models
 {
     public static partial class AssetController
     {
@@ -32,29 +29,6 @@ namespace AssetManagement.Models
         public static void UpdateAssetState(IAsset asset, AssetState assetState)
         {
             asset.Records.Add(new StateRecord(assetState));
-        }
-
-        // Temporary, I hope. Early mockdata function, pls remove when proper mockdata is done
-        public static Task<IAsset[]> GetAssetsAsync()
-        {
-            return Task.Factory.StartNew(() =>
-            {
-                Employee anna = new Employee("Anna", "anna@acme.dk");
-                Employee ulf = new Employee("Ulf", "ulf@acme.dk");
-
-                List<Asset> assets = new List<Asset>()
-                {
-                    new Asset(69420, "Epic Dell Gaming PC", "SN1"),
-                    new Asset(1234, "Not So Epic Dell Gaming PC", "SN1"),
-                    new Asset(4, "HP PC", "SN1"),
-                };
-
-                TransferOwnership(assets[0], ulf);
-                TransferOwnership(assets[1], ulf);
-                TransferOwnership(assets[2], anna);
-
-                return (IAsset[])assets.ToArray();
-            });
         }
     }
 }
--- AssetManagement/Data/AssetService.cs
+++ AssetManagement/Data/AssetService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using AssetManagement.Models;
 
 namespace AssetManagement
@@ -7,7 +8,24 @@ namespace AssetManagement
     {
         public Task<IAsset[]> GetAssetsAsync()
         {
-            return AssetController.GetAssetsAsync();
+            return Task.Factory.StartNew(() =>
+            {
+                Employee anna = new Employee("Anna", "anna@acme.dk");
+                Employee ulf = new Employee("Ulf", "ulf@acme.dk");
+
+                IAsset[] assets = new IAsset[]
+                {
+                    AssetController.MakeAsset(69420, "Epic Dell Gaming PC", "SN1"),
+                    AssetController.MakeAsset(1234, "Not So Epic Dell Gaming PC", "SN1"),
+                    AssetController.MakeAsset(4, "HP PC", "SN1"),
+                };
+
+                AssetController.TransferOwnership(assets[0], ulf);
+                AssetController.TransferOwnership(assets[1], ulf);
+                AssetController.TransferOwnership(assets[2], anna);
+
+                return assets;
+            });
         }
     }
 }
