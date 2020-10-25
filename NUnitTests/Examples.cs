5db4c02fa363cc9b65a49d67c788e8bef9e60569
Lasse Skaalum <lskaal19@student.aau.dk>
2020-10-25 22:05:45 +0100

    Fixed method names for tests according to the ms naming convention and fixed the mixed act&assert to be sepparated

--- NUnitTests/Examples.cs
+++ NUnitTests/Examples.cs
@@ -3,6 +3,11 @@ using System.Threading;
 using AssetManagement.Models;
 using NUnit.Framework;
 
+/* Method Naming Conventions. Should be sepparated by '_':
+ * - The name of the method being tested.
+ * - The scenario under which it's being tested.
+ * - The expected behavior when the scenario is invoked.
+ */
 namespace NUnitTests
 {
     public class Examples
@@ -15,8 +20,7 @@ namespace NUnitTests
 
         // TestCases can be used to test similar behavior without writing many repetitive tests.
         [TestCase(12345, "Epic Dell Gaming PC",     "SN1BFE4", "Ulf",    "ulf@acme.dk")]
-        
-        public void When_NewAsset_Expect_AssetCanBeTransferedToEmployee(int assetId, string assetName, string assetSerialNumber, 
+        public void TransferOwnership_AssetTransfer_AssetOwnershipTransfered(int assetId, string assetName, string assetSerialNumber, 
             string employeeName, string employeeEmail)
         {
             // Arrange
@@ -32,17 +36,16 @@ namespace NUnitTests
 
         // Testing if transfering ownership to the currentholder throws an exception
         [Test]
-        public void TransferOwnership_WhenTransferingAssetToTheHolderWhoAlreadyHasIt_ShouldThrowException()
+        public void TransferOwnership_TransferToCurrentHolder_ThrowException()
         {
             // Arrange
             IAsset asset = AssetController.MakeAsset(1234, "Testing computer", "abc123");
             Employee employee = new Employee("Bjarne", "Bjarne@testingCompany.com");
             AssetController.TransferOwnership(asset, employee);
-            
             // Act
-            
-            // Assert
-            Assert.Throws<ArgumentException>(() => AssetController.TransferOwnership(asset, employee));
+            TestDelegate shouldThrow = () => AssetController.TransferOwnership(asset, employee)
+            // Assert 
+            Assert.Throws<ArgumentException>(shouldThrow);
         }
     }
 }
