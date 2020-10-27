dfa00e76e0434628a95903569df90a906d3d2423
Pattrigue <pfas19@student.aau.dk>
2020-10-27 13:54:56 +0100

    Decouple AssetHolder from Asset. Transfer method now only creates a new StateRecord.

--- AssetManagement.Models/AssetController.Asset.cs
+++ AssetManagement.Models/AssetController.Asset.cs
@@ -14,7 +14,7 @@ namespace AssetManagement.Models
 
             public DateTime LastChanged { get; private set; }
 
-            public AssetHolder CurrentAssetHolder { get; private set; }
+            public AssetHolder CurrentAssetHolder => LastAssetRecord.CurrentHolder;
             public AssetRecord LastAssetRecord => AssetRecords[AssetRecords.Count - 1];
 
             public List<AssetRecord> AssetRecords { get; } = new List<AssetRecord>();
@@ -30,30 +30,8 @@ namespace AssetManagement.Models
 
             public void TransferTo(AssetHolder newAssetHolder)
             {
-                if (newAssetHolder.CurrentAssets.Contains(this))
-                {
-                    // ERROR: New holder is somehow already holding this asset!
-                    throw new ArgumentException("Attempt to add an asset to an asset holder's asset list which already contains this asset!", Model);
-                }
-
-                // Remove this asset from its current holder, if any
-                if (CurrentAssetHolder != null)
-                {
-                    if (!CurrentAssetHolder.CurrentAssets.Contains(this))
-                    {
-                        // ERROR: This asset does not exist in the CurrentAssetHolder's list so we cannot remove it!
-                        throw new ArgumentException($"Attempt to remove asset \"{Model}\" from an asset holder's asset list that did not contain the asset!");
-                    }
-
-                    CurrentAssetHolder.CurrentAssets.Remove(this);
-                }
-
                 // Record this transfer by adding a new transaction to the list of transactions
                 AssetRecords.Add(new AssetRecord(LastAssetRecord.State, newAssetHolder));
-
-                // Update the current holder by transferring this asset to the new AssetHolder
-                newAssetHolder.CurrentAssets.Add(this);
-                CurrentAssetHolder = newAssetHolder;
             }
 
             public void Dispose() { }
--- AssetManagement.Models/AssetHolder/AssetHolder.cs
+++ AssetManagement.Models/AssetHolder/AssetHolder.cs
@@ -1,29 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace AssetManagement.Models
+﻿namespace AssetManagement.Models
 {
     public abstract class AssetHolder
     {
         public string Name { get; private set; }
         public string Department { get; private set; }
-        public List<IAssetData> AssetList; // Init here or in ctor?
+
         public AssetHolder(string name) => Name = name;
-
-        /// <summary>
-        /// The list of assets currently being held by this asset holder.
-        /// </summary>
-        public List<IAssetData> CurrentAssets { get; } = new List<IAssetData>();
-
-        public virtual void RecieveAsset(IAssetData asset)
-        {
-            if (CurrentAssets.Contains(asset))
-            {
-                // ERROR: we're already holding this asset!
-                throw new ArgumentException("Attempt to add an asset to an asset holder's asset list which already contains this asset!", asset.Model);
-            }
-
-            CurrentAssets.Add(asset);
-        }
     }
 }
