f69ea82e80bc1d1c6130e9b586848ba4d3290aec
Pattrigue <pfas19@student.aau.dk>
2020-10-29 12:51:01 +0100

    Move IEmployee out of IAssetHolder.cs

--- AssetManagement.Core/IAssetHolder.cs
+++ AssetManagement.Core/IAssetHolder.cs
@@ -5,10 +5,4 @@
         // Corresponds to name for an employee or the display label for the cage or depot
         string Label { get; } 
     }
-
-    public interface IEmployee : IAssetHolder
-    {
-        string Username { get; }
-        string Department { get; }
-    }
 }
