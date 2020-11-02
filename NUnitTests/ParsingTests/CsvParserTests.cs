ebd8a02c35a691b3a35be800b104dde4882217f0
Lasse Skaalum <lskaal19@student.auu.dk>
2020-11-02 13:38:15 +0100

    Fixed path-problem in CsvLoader test

--- NUnitTests/ParsingTests/CsvParserTests.cs
+++ NUnitTests/ParsingTests/CsvParserTests.cs
@@ -20,24 +20,24 @@ namespace AssetManagement.NUnitTests
             Sepparator = ';';
             ActualLinesInCsvExcludingHeaderAndNewLineAtEndOfFile = 872;
         }
-        //[Test] // Uncomment this line to run the test again. The path doesn't work as it is now.
+        [Test] // Uncomment this line to run the test again. The path doesn't work as it is now.
         public void LoadDataFromCsv_LoadingData_LoadDataAndReturnListOfIAssets()
         {
             // Arrange
             CsvLoader loader = new CsvLoader();
-            FilePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "CompuerDataExample.csv"));
-            CsvLineParser skipParser =
-                (string originFileName, string[] fields, int expectedFieldsAmount, char sepparator) =>
-                {
-                    // This is where the parsing should've happened
-                    IAssetRecord assetRecord = Substitute.For<IAssetRecord>();
+            // TestDirectory is this dir: p3\NUnitTests\bin\Debug\netcoreapp3.1
+            FilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "test-files", "CompuerDataExample.csv");
+            CsvLineParser skipParser = (string originFileName, string[] fields, int expectedFieldsAmount, char sepparator) =>
+            {
+                // This is where the parsing should've happened
+                IAssetRecord assetRecord = Substitute.For<IAssetRecord>();
 
-                    return assetRecord;
-                };
+                return assetRecord;
+            };
             // Act
             List<IAssetRecord> parsedData = loader.LoadDataFromCsv(FilePath, ExpectedFieldsAmount, Sepparator, skipParser);
             // Assert
-            Assert.Equals(ActualLinesInCsvExcludingHeaderAndNewLineAtEndOfFile, parsedData.Count);
+            Assert.AreEqual(ActualLinesInCsvExcludingHeaderAndNewLineAtEndOfFile, parsedData.Count);
         }
     }
 }
