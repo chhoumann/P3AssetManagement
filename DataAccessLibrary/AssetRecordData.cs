061a8d52d175e0d742c3bf40a9d48df9958eb200
Laurits <laurits.mumberg@gmail.com>
2020-11-09 11:33:43 +0100

    Pushed changes to assetRecordData. Now has AssetId

--- DataAccessLibrary/AssetRecordData.cs
+++ DataAccessLibrary/AssetRecordData.cs
@@ -9,14 +9,16 @@ namespace AssetManagement.DataAccessLibrary
     public class AssetRecordData
     {
         // Properties must be both get and set for EntityFrameworkCore to create the object 
-        string FileName { get; set; }
-        DateTime Date { get; set; }
-        IAssetHolder Holder { get; set; }
-        AssetState State { get; set; }
+        public string FileName { get; set; }
+        public DateTime Date { get; set; }
+        public string AssetId { get; set; }
+        public IAssetHolder Holder { get; set; }
+        public AssetState State { get; set; }
 
         public AssetRecordData(IAssetRecord assetRecord)
         {
             FileName = assetRecord.FileName;
+            AssetId = assetRecord.AssetId;
             Date = assetRecord.Date;
             Holder = assetRecord.Holder;
             State = assetRecord.State;
