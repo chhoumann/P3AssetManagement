8f89852b7c1f43b8969da1873bb04e1f3b8c5f4d
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-11-10 13:46:02 +0100

    AssetDetails table navigation

--- Server/Pages/AssetDetails.razor.cs
+++ Server/Pages/AssetDetails.razor.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AssetManagement.Core;
+using AssetManagement.Models;
+using AssetManagement.Server.Components;
 using Microsoft.AspNetCore.Components;
 
 namespace AssetManagement.Server.Pages
@@ -8,12 +10,28 @@ namespace AssetManagement.Server.Pages
     public sealed partial class AssetDetails
     {
         [Parameter] public int AssetId { get; set; }
-        public IAsset Asset { get; set; }
+
+        private IAsset asset { get; set; }
+        private IAssetRecord[] assetRecords;
+        private IAssetRecord[] pageAssetRecords;
+
+        private PageNavigator<IAssetRecord> navigator;
+
+        private const int AssetsPerPage = 10;
 
         protected override async Task OnInitializedAsync()
         {
-            Asset = await AssetService.GetSingleAssetAsync(AssetId);
+            asset = await AssetService.GetSingleAssetAsync(AssetId);
 
+            assetRecords = asset.AssetRecords.ToArray();
+
+            navigator = new PageNavigator<IAssetRecord>(assetRecords, out pageAssetRecords, AssetsPerPage);
+            navigator.OnPageChanged += GetAssetRecords;
+        }
+
+        private void GetAssetRecords(IAssetRecord[] pageAssetRecords)
+        {
+            this.pageAssetRecords = pageAssetRecords;
         }
     }
 }
