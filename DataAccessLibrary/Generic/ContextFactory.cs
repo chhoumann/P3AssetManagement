0f58fbbe33f67c74b602d30aad1081faa4b58ef7
Pattrigue <pfas19@student.aau.dk>
2020-11-12 12:41:12 +0100

    Begin work on generic DB context and data access library

---
+++ DataAccessLibrary/Generic/ContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace AssetManagement.DataAccessLibrary.Generic
+{
+    public sealed class ContextFactory<T>: DbContext where T : class
+    {
+        public DbSet<T> Data { get; set; }
+
+        private readonly string connectionStringKey = "p3db";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                string connectionString = configuration.GetConnectionString(connectionStringKey);
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
