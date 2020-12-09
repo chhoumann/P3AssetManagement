3c72e7b22d3167fd036fdf97013bb239c2e2cbfe
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-12-09 11:24:22 +0100

    Refactor (#85)
    Co-authored-by: Pattrigue <57709490+Pattrigue@users.noreply.github.com>

--- Models/DataLoadStrategy/ComputerCsvLoader.cs
+++ Models/DataLoadStrategy/ComputerCsvLoader.cs
@@ -19,6 +19,16 @@ namespace AssetManagement.Models.DataLoadStrategy
         
         public override IEnumerable<Computer> ReadData(string filePath)
         {
+            WaitForFileToBeReadable(filePath);
+
+            return File
+                .ReadAllLines(filePath)
+                .Skip(1)
+                .Select(CreateComputerFromCsvLine);
+        }
+
+        private void WaitForFileToBeReadable(string filePath)
+        {
             uint numReadAttempts = 0;
 
             while (numReadAttempts++ < MaxFileReadAttempts && !IsFileReady(filePath))
@@ -31,22 +41,19 @@ namespace AssetManagement.Models.DataLoadStrategy
                 // TODO: The caller of this method should catch this exception
                 throw new FileNotReadException($"CSV file could not be read at path {filePath}!");
             }
+        }
 
-            return File
-                .ReadAllLines(filePath)
-                .Skip(1)
-                .Select(csvLine =>
-                {
-                    string[] fields = csvLine.Split(Separator);
+        private Computer CreateComputerFromCsvLine(string csvLine)
+        {
+            string[] fields = csvLine.Split(Separator);
 
-                    Computer computer = new Computer(fields[4], fields[5], fields[6],
-                        fields[7], fields[9]);
-                    AssetHolder assetHolder = new AssetHolder(fields[2], fields[1], fields[3]);
-                    
-                    computer.ComputerRecords.Add(new ComputerRecord(computer, assetHolder, DateTime.Now, AssetState.Online));
-                    
-                    return computer;
-                });
+            Computer computer = new Computer(fields[4], fields[5], fields[6],
+                fields[7], fields[9]);
+            AssetHolder assetHolder = new AssetHolder(fields[2], fields[1], fields[3]);
+
+            computer.ComputerRecords.Add(new ComputerRecord(computer, assetHolder, DateTime.Now, AssetState.Online));
+
+            return computer;
         }
     }
 }
