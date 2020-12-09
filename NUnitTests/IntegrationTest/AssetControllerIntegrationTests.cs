02c5ab8e311e45b33601f51376e73af8587319c0
Laurits <laurits.mumberg@gmail.com>
2020-12-09 10:18:12 +0100

    AssetController intergration tests

--- NUnitTests/IntegrationTest/AssetControllerIntegrationTests.cs
+++ NUnitTests/IntegrationTest/AssetControllerIntegrationTests.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using AssetManagement.Core;
-using AssetManagement.Core.DataLoadStrategy;
 using AssetManagement.DataAccessLibrary;
 using AssetManagement.DataAccessLibrary.DataModels;
 using AssetManagement.DataAccessLibrary.DataModels.Interfaces;
@@ -13,12 +9,17 @@ using AssetManagement.Models;
 using AssetManagement.Models.DataLoadStrategy;
 using NUnit.Framework;
 
-namespace AssetManagement.NUnitTests.IntegrationTests
+namespace AssetManagement.NUnitTests.IntegrationTest
 {
     public sealed class MockComputerService : IAssetService<Computer>
     {
+        public static List<Computer> computersRecievedFromController = new List<Computer>();
         public event Action AssetUpdated;
-        public Computer[] GetAssets() => throw new NotImplementedException();
+        public AssetHolder Cage { get; }
+        public AssetHolder Depot { get; }
+
+        public Computer[] GetAssets() => new ComputerCsvLoader(';').ReadData(Path.Combine(Environment.CurrentDirectory, 
+            "test-files", "asset-controller-test", "2020-10-07-PCID.csv")).ToArray();
 
         public Computer GetAssetById(string id) => throw new NotImplementedException();
 
@@ -29,7 +30,7 @@ namespace AssetManagement.NUnitTests.IntegrationTests
 
         public void AddAssets(IEnumerable<Computer> assets)
         {
-            throw new NotImplementedException();
+            computersRecievedFromController.AddRange(assets);
         }
 
         public void DeleteAsset(Computer asset)
@@ -39,16 +40,26 @@ namespace AssetManagement.NUnitTests.IntegrationTests
     }
     public class AssetControllerIntegrationTests
     {
+        private const int FileReadTimeout = 5;
         
-        string filePathToBFolder = Path.Combine(Environment.CurrentDirectory, 
+        // Filepaths for FileWatcher test
+        private readonly string filePathToBFolder = Path.Combine(Environment.CurrentDirectory, 
             "test-files", "file-watcher-test", "toB");
         
-        string filePathFromA = Path.Combine(Environment.CurrentDirectory, 
-                                "test-files", "file-watcher-test", "fromA", "2020-10-07-PCID.csv");
-        string filePathToB = Path.Combine(Environment.CurrentDirectory, 
+        private readonly string filePathFromA = Path.Combine(Environment.CurrentDirectory, 
+            "test-files", "file-watcher-test", "fromA", "2020-10-07-PCID.csv");
+        private readonly string filePathToB = Path.Combine(Environment.CurrentDirectory, 
             "test-files", "file-watcher-test", "toB", "2020-10-07-PCID.csv");
         
-        // Succes: Vi har en funktion som subscriber til FileRead fra AafComputerCsvFileWatcher, der bliver invoket.
+        // Filepaths for AssetController test
+        private readonly string filePathToDFolder = Path.Combine(Environment.CurrentDirectory, 
+            "test-files", "asset-controller-test", "toD");
+        
+        private readonly string filePathFromC = Path.Combine(Environment.CurrentDirectory, 
+            "test-files", "asset-controller-test", "fromC", "2020-10-27-PCID.csv");
+        private readonly string filePathToD = Path.Combine(Environment.CurrentDirectory, 
+            "test-files", "asset-controller-test", "toD", "2020-10-27-PCID.csv");
+        
         [Test]
         public void AafComputerCsvFileWatcher_NewDataReceived_FileReadEventIsCalled()
         {
@@ -69,7 +80,7 @@ namespace AssetManagement.NUnitTests.IntegrationTests
             File.Copy(filePathFromA,filePathToB);
 
             // Assert
-            Assert.That(() => hasBeenCallled, Is.True.After(5).Seconds.PollEvery(500).MilliSeconds);
+            Assert.That(() => hasBeenCallled, Is.True.After(FileReadTimeout).Seconds.PollEvery(500).MilliSeconds);
         }
 
         [Test]
@@ -95,25 +106,25 @@ namespace AssetManagement.NUnitTests.IntegrationTests
             // Assert
             Assert.That(currentAssets[0].LastAssetRecord.Holder, Is.EqualTo(assetHolder2));
         }
-        //
-        // [Test]
-        // public void Method()
-        // {
-        //     // Arrange
-        //     AafComputerCsvFileWatcher fileWatcher = new AafComputerCsvFileWatcher(filePathToBFolder, new ComputerCsvLoader(';'));
-        //     AssetController<Computer, MockComputerService> assetController = new AssetController<Computer, MockComputerService>()
-        //         .StartWatchingAlienData(fileWatcher);
-        //     
-        //     // Act
-        //     
-        //     if (File.Exists(filePathToB))
-        //     {
-        //         File.Delete(filePathToB);
-        //     }
-        //     File.Copy(filePathFromA,filePathToB);
-        //     
-        //     // Assert
-        //     Assert.IsTrue(true); // TODO - Finish this
-        // }
+        
+        [Test]
+        public void AssetController_NewDataRecievedWithOneNewUser_OneAssetAddedToIAssetServices()
+        {
+            // Arrange
+            AafComputerCsvFileWatcher fileWatcher = 
+                new AafComputerCsvFileWatcher(filePathToDFolder, new ComputerCsvLoader(';'));
+            new AssetController<Computer, MockComputerService>().StartWatchingAlienData(fileWatcher);
+            
+            // Act
+            if (File.Exists(filePathToD))
+            {
+                File.Delete(filePathToD);
+            }
+            File.Copy(filePathFromC,filePathToD);
+            
+            // Assert
+            Assert.That(() => (MockComputerService.computersRecievedFromController.Count == 1), 
+                Is.True.After(FileReadTimeout).Seconds.PollEvery(500).MilliSeconds);
+        }
     }
 }
