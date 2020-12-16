a5cb67fb7f8ee55c12709a5a2f9f0088e0179cbf
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-12-16 13:43:29 +0100

    Move IStaticAssetHolders to own file

---
+++ DataAccessLibrary/IStaticAssetHolders.cs
@@ -0,0 +1,10 @@
+﻿using AssetManagement.DataAccessLibrary.DataModels;
+
+namespace AssetManagement.DataAccessLibrary
+{
+    public interface IStaticAssetHolders
+    {
+        AssetHolder Depot { get; }
+        AssetHolder Cage { get; }
+    }
+}
